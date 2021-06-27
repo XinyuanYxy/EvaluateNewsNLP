460fc4a4f5a0a8d21fc03f9772830079d3fe7b6a
Yxychris <yxychris@outlook.com>
2021-06-27 17:17:21 -0400

    added url validation

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,23 +2,25 @@ async function handleSubmit(event) {
 	event.preventDefault();
 	console.log('::: Form Submitted :::');
 	// check what text was put into the form field
-	let formText = document.getElementById('name').value;
-	Client.checkForName(formText);
-
-	try {
-		await axios
-			.get(`http://localhost:8080/sentimentAnalysis/${formText}`)
-			.then(function (res) {
-				document.getElementById(
-					'results'
-				).innerHTML = `<div>confidence: ${res.data.confidence}<div>
-			<div>agreement: ${res.data.agreement}<div>
-			<div>irony: ${res.data.irony}<div>
-			<div>model: ${res.data.model}<div>
-			<div>score_tag: ${res.data.score_tag}<div>`;
-			});
-	} catch (error) {
-		console.log(error);
+	let formText = document.getElementById('txt').value;
+	if (Client.checkForInput(formText)) {
+		try {
+			await axios
+				.get(`http://localhost:8080/sentimentAnalysis/${formText}`)
+				.then(function (res) {
+					document.getElementById(
+						'results'
+					).innerHTML = `<div>confidence: ${res.data.confidence}<div>
+				<div>agreement: ${res.data.agreement}<div>
+				<div>irony: ${res.data.irony}<div>
+				<div>model: ${res.data.model}<div>
+				<div>score_tag: ${res.data.score_tag}<div>`;
+				});
+		} catch (error) {
+			console.log(error);
+		}
+	} else {
+		alert('please enter a valid input');
 	}
 }
 
